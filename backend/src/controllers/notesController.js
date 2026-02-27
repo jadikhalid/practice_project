403b0681f2d185ff0866432b31f84cb293328fdc
Jadi Khalid <jadikhalid@gmail.com>
2026-02-27 12:17:04 +0000

    ras

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,15 +1,56 @@
-export const getAllNotes = (req, res) => {
-  res.status(200).json({ message: "All notes retrieved successfully!" });
+import Note from "../models/Note.js";
+
+export const getAllNotes = async (req, res) => {
+  try {
+    const notes = await Note.find();
+    res.status(200).json(notes);
+  } catch (error) {
+    console.log("Error in getAllNotes", error);
+    res.status(500).json({ message: "Error retrieving notes", error });
+  }
 };
 
-export const createNote = (req, res) => {
-  res.status(201).json({ message: "Note created successfully!" });
+export const createNote = async (req, res) => {
+  try {
+    const { title, content } = req.body;
+    const newNote = new Note({ title, content });
+    await newNote.save();
+    res.status(201).json(newNote);
+  } catch (error) {
+    console.log("Error in createNote", error);
+    res.status(500).json({ message: "Error creating note", error });
+  }
 };
 
-export const updateNote = (req, res) => {
-  res.status(200).json({ message: "Note updated successfully!" });
+export const updateNote = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const updatedNote = await Note.findByIdAndUpdate(
+      id,
+      { title, content },
+      { new: true },
+    );
+    if (!updatedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    res.status(200).json(updatedNote);
+  } catch (error) {
+    console.log("Error in updateNote", error);
+    res.status(500).json({ message: "Error updating note", error });
+  }
 };
 
-export const deleteNote = (req, res) => {
-  res.status(200).json({ message: "Note deleted successfully!" });
+export const deleteNote = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedNote = await Note.findByIdAndDelete(id);
+    if (!deletedNote) {
+      return res.status(404).json({ message: "Note not found" });
+    }
+    res.status(200).json({ message: "Note deleted successfully!" });
+  } catch (error) {
+    console.log("Error in deleteNote", error);
+    res.status(500).json({ message: "Error deleting note", error });
+  }
 };
